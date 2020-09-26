a487a62c66a0c6278418745e0a958c11786c7708
Stephen Dade <stephen_dade@hotmail.com>
2020-09-26 16:25:21 +1000

    Camera: Added ArduCam OV9281

---
+++ lib/cameraArduCamUC580.py
@@ -0,0 +1,67 @@
+'''
+Camera Interfacing for the ArduCam UC-580 (OV9281 Global Shutter)
+'''
+
+import numpy
+import cv2
+import time
+import arducam_mipicamera as arducam
+
+
+class cameraUC580:
+    '''A Camera setup and capture class for the ArduCam UC580'''
+    
+    def __init__(self, camParams):
+        '''Initialise the camera, based on a dict of settings'''
+        
+        if camParams['resolution'][0] % 16 != 0 or camParams['resolution'][1] % 16 != 0:
+            print("Error: Camera resolution must be divisible by 16")
+            return
+            
+        self.camParams = camParams
+        self.camera = arducam.mipi_camera()
+        self.camera.rotation = camParams['rotation']
+        
+        self.V4L2_CID_EXPOSURE = 9963793
+        
+        # Set camera settings
+        self.camera.init_camera()
+        self.camera.set_resolution(self.camParams['resolution'][0], self.camParams['resolution'][1])
+        self.camera.set_control(self.V4L2_CID_EXPOSURE, 600)
+        
+        fmt = self.camera.get_format()
+        print("Camera format is {}".format(fmt))
+        
+        time.sleep(1)
+        
+    def getNumberImages(self):
+        '''Get number of loaded images'''
+        return None
+
+    def getFileName(self):
+        '''Get current file in camera'''
+        return None
+        
+    def getImage(self):
+        ''' Capture a single image from the Camera '''
+        
+        frame = self.camera.capture(encoding = "i420")
+        image = frame.as_array.reshape(int(self.camera.resolution[1]*1.5), self.camera.resolution[0])
+        del frame
+        
+        # Convert to greyscale and crop
+        cv2.cvtColor(image, cv2.COLOR_YUV2GRAY_I420)
+        imageCrop = image[0:self.camera.resolution[1], 0:self.camera.resolution[0]]
+        
+        # Rotate if required
+        if self.camParams['rotation'] == 180:
+            imageCrop = cv2.rotate(imageCrop, cv2.ROTATE_180)
+        if self.camParams['rotation'] == 90:
+            imageCrop = cv2.rotate(imageCrop, cv2.ROTATE_90_CLOCKWISE)
+        if self.camParams['rotation'] == 270:
+            imageCrop = cv2.rotate(imageCrop, cv2.ROTATE_90_COUNTERCLOCKWISE)            
+        return imageCrop
+        
+    def close(self):
+        ''' close the camera'''
+        self.camera.close_camera()

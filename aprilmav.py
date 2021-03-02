a154c096c8a8fc8333a5bf345859b24540771643
Stephen Dade <stephen_dade@hotmail.com>
2021-03-02 20:09:47 +1100

    Aprilmav: Add save option

--- aprilmav.py
+++ aprilmav.py
@@ -10,6 +10,8 @@ import yaml
 import argparse
 import threading
 import signal
+import queue
+import os
 from collections import deque
 from statistics import mean
 
@@ -22,6 +24,59 @@ exit_event = threading.Event()
 
 def signal_handler(signum, frame):
     exit_event.set()
+
+
+# Separate thread for saving images, in order to not delay image capture
+class saveThread(threading.Thread):
+    def __init__(self, folder):
+        threading.Thread.__init__(self)
+        self.save_queue = queue.Queue()
+        
+        #create the capture folder if required
+        try:
+            os.makedirs(os.path.join(".", folder))
+        except FileExistsError:
+            pass
+        
+    def run(self):
+        while True:
+            if self.save_queue.empty():
+                continue
+            (image, filename, posn, rot, tags) = self.save_queue.get()
+            # add in data (colour)
+            imageColour = cv2.cvtColor(image,cv2.COLOR_GRAY2RGB)
+            #outfile.write("{0},{1:.3f},{2:.3f},{3:.3f},{4:.1f},{5:.1f},{6:.1f}\n".format(file, posn[0], posn[1], posn[2], rot[0], rot[1], rot[2]))
+            cv2.putText(imageColour, "Pos (m) = {0:.3f}, {1:.3f}, {2:.3f}".format(posn[0], posn[1], posn[2]), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+            cv2.putText(imageColour, "Rot (deg) = {0:.1f}, {1:.1f}, {2:.1f}".format(rot[0], rot[1], rot[2]), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+            imageColour = self.labelTags(imageColour, tags)
+            cv2.imwrite(filename, imageColour, [cv2.IMWRITE_JPEG_QUALITY, 99])
+
+            #print("Saved {0}".format(filename))
+            if exit_event.wait(timeout=0.1):
+                return
+                
+    def labelTags(self, image, tags):
+        # Label the tags in the image
+        # loop over the AprilTag detection results
+        for r in tags:
+	        # extract the bounding box (x, y)-coordinates for the AprilTag
+	        # and convert each of the (x, y)-coordinate pairs to integers
+	        (ptA, ptB, ptC, ptD) = r.corners
+	        ptB = (int(ptB[0]), int(ptB[1]))
+	        ptC = (int(ptC[0]), int(ptC[1]))
+	        ptD = (int(ptD[0]), int(ptD[1]))
+	        ptA = (int(ptA[0]), int(ptA[1]))
+	        # draw the bounding box of the AprilTag detection
+	        cv2.line(image, ptA, ptB, (0, 255, 0), 2)
+	        cv2.line(image, ptB, ptC, (0, 255, 0), 2)
+	        cv2.line(image, ptC, ptD, (0, 255, 0), 2)
+	        cv2.line(image, ptD, ptA, (0, 255, 0), 2)
+	        # draw the center (x, y)-coordinates of the AprilTag
+	        (cX, cY) = (int(r.center[0]), int(r.center[1]))
+	        cv2.circle(image, (cX, cY), 5, (0, 0, 255), -1)
+	        # draw the tag ID 
+	        cv2.putText(image, str(r.tag_id), (ptA[0] + 10, ptA[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 1)
+	        return image
     
 class statusThread(threading.Thread):
     def __init__(self):
@@ -56,6 +111,7 @@ class mavThread(threading.Thread):
         self.timestamp = 0
         self.lock = threading.Lock()
         self.conn = None
+        self.goodToSend = False
         
     def run(self):
         # Start mavlink connection
@@ -70,6 +126,7 @@ class mavThread(threading.Thread):
         while True:
             if self.conn.wait_heartbeat(timeout=0.5) != None:
                 # Got a hearbeart, go to next loop
+                self.goodToSend = True
                 break
             if exit_event.is_set():
                 return
@@ -93,8 +150,11 @@ class mavThread(threading.Thread):
         # https://mavlink.io/en/messages/common.html#VISION_POSITION_ESTIMATE
         # Frame is NED?
         #if self.getTimestamp() > 0:
-        self.conn.mav.vision_position_estimate_send(t, x, y, z, roll, pitch, yaw, covariance=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], reset_counter=0)
-        return True
+        if self.goodToSend:
+            self.conn.mav.vision_position_estimate_send(t, x, y, z, roll, pitch, yaw, covariance=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], reset_counter=0)
+            return True
+        else:
+            return False
         #else:
         #    #print("Can't send")
         #    return False
@@ -108,6 +168,7 @@ if __name__ == '__main__':
     parser.add_argument("-device", type=str, default="udpin:127.0.0.1:14550", help="MAVLink connection string")
     parser.add_argument("-baud", type=int, default=115200, help="MAVLink baud rate, if using serial")
     parser.add_argument("-source-system", type=int, default=255, help="MAVLink Source system")
+    parser.add_argument("-imageFolder", type=str, default="", help="Save processed images to this folder")
     args = parser.parse_args()
     
     print("Initialising")
@@ -129,7 +190,7 @@ if __name__ == '__main__':
 
     # allow the camera to warmup
     time.sleep(2)
-
+    
     at_detector = Detector(searchpath=['apriltags3py/apriltags/lib', 'apriltags3py/apriltags/lib'],
                            families='tagStandard41h12',
                            nthreads=3,
@@ -168,7 +229,14 @@ if __name__ == '__main__':
     # Start Status thread
     threadStatus = statusThread()
     threadStatus.start()
+    
+    # Start save image thread, if desired
+    threadSave = None
+    if args.imageFolder != "":
+        threadSave = saveThread(args.imageFolder)
+        threadSave.start()        
         
+    i = 0
     while True:
         #print("--------------------------------------")
         
@@ -179,6 +247,7 @@ if __name__ == '__main__':
         timestamp = int(round(time.time() * 1000000))
         #print("Timestamp of capture = {0}".format(timestamp))
         imageBW = camera.getImage()
+        i += 1
         
         # we're out of images
         if imageBW is None:
@@ -217,6 +286,10 @@ if __name__ == '__main__':
         
         # Send to status thread
         threadStatus.updateData(time.time() - myStart, (posn[0], posn[1], posn[2]), wasSent)
+        
+        # Send to save thread
+        if threadSave:
+            threadSave.save_queue.put((imageBW, os.path.join(".", args.imageFolder, "processed_{:04d}.jpg".format(i)), posn, rot, tags))
         
         # Get ready for next frame
         tagPlacement.newFrame()

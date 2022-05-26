c5f2061546983af84dcc63d69843336441131558
Stephen Dade <stephen_dade@hotmail.com>
2022-05-26 16:04:13 +1000

    Fix ArduCam install

--- lib/getArduCamfiles.sh
+++ lib/getArduCamfiles.sh
@@ -2,4 +2,5 @@
 
 wget https://raw.githubusercontent.com/ArduCAM/MIPI_Camera/master/RPI/python/arducam_mipicamera.py
 wget https://github.com/ArduCAM/MIPI_Camera/raw/master/RPI/lib/libarducam_mipicamera.so
-
+sudo cp libarducam_mipicamera.so /usr/lib/
+sudo ldconfig

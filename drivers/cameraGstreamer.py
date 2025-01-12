b15f1a98323a4883e2364f171bc316c101cbb383
Stephen Dade <stephen_dade@hotmail.com>
2025-01-12 21:03:00 +1100

    Move camera fisheye mapping to drivers

--- drivers/cameraGstreamer.py
+++ drivers/cameraGstreamer.py
@@ -4,6 +4,7 @@ Camera Interfacing for a GStreamer pipeline via OpenCV
 
 import time
 import cv2
+import numpy
 
 
 class camera:
@@ -22,6 +23,24 @@ class camera:
         if 'GStreamer:                   YES' not in cv2.getBuildInformation():
             print("Error: OpenCV is not built with GStreamer support")
             return
+
+        # Need to reconstruct K and D for each camera
+        self.K = numpy.zeros((3, 3))
+        self.D = numpy.zeros((4, 1))
+        self.fisheye = camParams['fisheye']
+        self.dim1 = None
+        self.map1 = None
+        self.map2 = None
+        if camParams['fisheye']:
+            self.K[0, 0] = camParams['cam_params'][0]
+            self.K[1, 1] = camParams['cam_params'][1]
+            self.K[0, 2] = camParams['cam_params'][2]
+            self.K[1, 2] = camParams['cam_params'][3]
+            self.K[2, 2] = 1
+            self.D[0][0] = camParams['cam_paramsD'][0]
+            self.D[1][0] = camParams['cam_paramsD'][1]
+            self.D[2][0] = camParams['cam_paramsD'][2]
+            self.D[3][0] = camParams['cam_paramsD'][3]
 
         # Construct the GStreamer pipeline string
         gst_pipeline = (
@@ -54,8 +73,17 @@ class camera:
 
         timestamp = time.time()
         return_value, image = self.camera.read()
+        imageBW = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        return (cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), timestamp)
+        # Generate the undistorted image mapping if fisheye
+        if self.fisheye and self.dim1 is None:
+            # Only need to get mapping at first frame
+            # dim1 is the dimension of input image to un-distort
+            self.dim1 = imageBW.shape[:2][::-1]
+            self.map1, self.map2 = cv2.fisheye.initUndistortRectifyMap(
+                self.K, self.D, numpy.eye(3), self.K, self.dim1, cv2.CV_16SC2)
+
+        return (imageBW, timestamp)
 
     def close(self):
         ''' close the camera'''

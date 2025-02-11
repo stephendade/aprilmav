9aa482d68d8bd4c56189d2688420a2ce7010dd2d
Stephen Dade <stephen_dade@hotmail.com>
2025-02-11 20:06:29 +1100

    Process: support multiple cameras

--- process_test.py
+++ process_test.py
@@ -8,45 +8,83 @@ Distance is relative to the camera's sensor in 3 dimensions.
 
 '''
 
+import signal
+import threading
 import time
 import argparse
 import os
 from collections import defaultdict
+import concurrent.futures
 
 import numpy
-
 from pyapriltags import Detector
+
 from modules.geo import getPos, getTransform, getRotation
 from modules.common import loadCameras
 
+exit_event = threading.Event()
 
-def main(args):
+
+def signal_handler(signum, frame):
+    """
+    Signal handler for exit
+    """
+    exit_event.set()
+
+
+def capture_image(CAMERA):
+    """
+    Captures an image using the provided CAMERA object.
+
+    Args:
+        CAMERA: An object representing the camera, which must have the methods
+                getFileName() and getImage().
+
+    Returns:
+        tuple: A tuple containing:
+            - camName (str): The name of the camera.
+            - imageBW (numpy.ndarray or None): The captured image in black and white, or None if no image is captured.
+            - filename (str or None): The filename of the captured image, or None if no image is captured.
+    """
+    filename = CAMERA.getFileName()
+    (imageBW, _) = CAMERA.getImage()
+
+    # we're out of images
+    if imageBW is None:
+        return CAMERA.camName, None, None
+
+    return CAMERA.camName, imageBW, filename
+
+
+def main(mainargs):
     print("Initialising")
 
     # Open camera settings and load camera(s)
-    CAMERAS = loadCameras(args.multiCamera, args.camera, args.inputFolder, args.jetson)
+    CAMERAS = loadCameras(mainargs.multiCamera, mainargs.camera, mainargs.inputFolder, mainargs.jetson)
 
     # allow the camera to warmup
     time.sleep(2)
 
     at_detector = Detector(searchpath=['apriltags3py/apriltags/lib', 'apriltags3py/apriltags/lib'],
-                           families=args.tagFamily,
+                           families=mainargs.tagFamily,
                            nthreads=max(1, os.cpu_count() - 1),
-                           quad_decimate=args.decimation,
+                           quad_decimate=mainargs.decimation,
                            quad_sigma=0.0,
                            refine_edges=1,
                            decode_sharpening=0.25,
                            debug=0)
 
     # how many loops. If using a file input, just use first camera
-    loops = CAMERAS[0].getNumberImages() if CAMERAS[0].getNumberImages() else args.loop
+    loops = CAMERAS[0].getNumberImages() if CAMERAS[0].getNumberImages() else mainargs.loop
 
     print("Starting {0} image capture and process...".format(loops))
+    signal.signal(signal.SIGINT, signal_handler)
 
-    with open(args.outFile, "w+", encoding="utf-8") as outFile:
-        outFile.write("{0},{1},{2},{3},{4},{5},{6},{7},{8}\n".format("Filename", "TagID", "PosX (left)", "PosY (up)",
-                                                                    "PosZ (fwd)", "RotX (pitch)", "RotY (yaw)",
-                                                                    "RotZ (roll)", "PoseErr"))
+    with open(mainargs.outFile, "w+", encoding="utf-8") as outFile:
+        outFile.write("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}\n".format("Filename", "CameraName", "TagID",
+                                                                         "PosX (fwd)", "PosY (right)", "PosZ (down)",
+                                                                         "RotX (roll)", "RotY (pitch)", "RotZ (yaw)",
+                                                                         "PoseErr"))
 
     # hold all pose errors to get average at end:
     all_pose_error = []
@@ -54,50 +92,64 @@ def main(args):
 
     for i in range(loops):
         print("--------------------------------------")
-        # grab an image from the cameras
-        tags = []
-        image_timestamp_camera = []
+        # Capture images from all cameras (in parallel)
+        img_by_cam = {}
+        tags_by_cam = {}
+
+        timestamp = time.time()
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = {executor.submit(capture_image, CAMERA): CAMERA for CAMERA in CAMERAS}
+            for future in concurrent.futures.as_completed(futures):
+                cam_name, imageBW, filename = future.result()
+                if imageBW is not None:
+                    img_by_cam[cam_name] = imageBW
+                    # print("Camera {0} capture time is {1:.1f}ms".format(cam_name, 1000*(time.time() - timestamp)))
+                else:
+                    break
+
+        # Detect tags in each camera
         for CAMERA in CAMERAS:
-            file = CAMERA.getFileName()
-            (imageBW, timestamp) = CAMERA.getImage()
-
-            # we're out of images
-            if imageBW is None:
-                break
-
-            image_timestamp_camera.append((imageBW, timestamp, CAMERA.camParams['cam_params']))
-
-        # do Apriltag detection and pose estimation on all captured images
-        for imageBW, timestamp, camParam in image_timestamp_camera:
             # AprilDetect, after accounting for distortion  (if fisheye)
-            tags.extend(at_detector.detect(imageBW, True, camParam, args.tagSize/1000))
+            tags = at_detector.detect(img_by_cam[CAMERA.camName], True, CAMERA.KFlat, mainargs.tagSize/1000)
+            tags_by_cam[CAMERA.camName] = tags
+            if filename:
+                print("File: {0} ({1}/{2})".format(filename, i, loops))
 
-            if file:
-                print("File: {0} ({1}/{2})".format(file, i, loops))
+        # get time to capture and convert
+        print("Time to capture and detect = {0:.1f} ms. ".format(1000*(time.time() - timestamp)))
+        for CAMERA in CAMERAS:
+            print("Camera {0} found {1} tags. ".format(CAMERA.camName, len(tags_by_cam[CAMERA.camName])))
 
-            # get time to capture and convert
-            print("Time to capture and detect = {0:.1f} ms, found {1} tags".format(
-                1000*(time.time() - timestamp), len(tags)))
+        # Convert to vehicle frame and add to list
+        for CAMERA in CAMERAS:
+            for tag in tags_by_cam[CAMERA.camName]:
+                tag_Veh = CAMERA.T_CamtoVeh @ getTransform(tag)
 
-        for tag in tags:
+                tagpos = getPos(tag_Veh)
+                tagrot = getRotation(tag_Veh)
+                all_pose_error.append(tag.pose_err*1E8)
+                all_tags[tag.tag_id].append(tagpos)
 
-            tagpos = getPos(getTransform(tag))
-            tagrot = getRotation(getTransform(tag))
-            all_pose_error.append(tag.pose_err*1E8)
-            all_tags[tag.tag_id].append(tagpos)
+                print("Cam {0}, Tag {1} pos = {2} m, Rot = {3} deg. ErrE8 = {4:.4f}".format(CAMERA.camName,
+                                                                                            tag.tag_id,
+                                                                                            tagpos.round(3),
+                                                                                            tagrot.round(1),
+                                                                                            tag.pose_err*1E8))
+                with open(mainargs.outFile, "a", encoding="utf-8") as outFile:
+                    outFile.write("{0},{1},{2},".format(filename, CAMERA.camName, tag.tag_id))
+                    outFile.write("{0:.3f},{1:.3f},{2:.3f},{3:.1f},{4:.1f},{5:.1f},{6}\n".format(tagpos[0],
+                                                                                                 tagpos[1],
+                                                                                                 tagpos[2],
+                                                                                                 tagrot[0],
+                                                                                                 tagrot[1],
+                                                                                                 tagrot[2],
+                                                                                                 tag.pose_err))
 
-            print("Tag {0} pos = {1} m, Rot = {2} deg. ErrE8 = {3:.4f}".format(tag.tag_id, tagpos.round(3),
-                                                                               tagrot.round(1), tag.pose_err*1E8))
-            with open(args.outFile, "w+", encoding="utf-8") as outFile:
-                outFile.write("{0},{1},{2:.3f},{3:.3f},{4:.3f},{5:.1f},{6:.1f},{7:.1f},{8}\n".format(file,
-                                                                                                     tag.tag_id,
-                                                                                                     tagpos[0],
-                                                                                                     tagpos[1],
-                                                                                                     tagpos[2],
-                                                                                                     tagrot[0],
-                                                                                                     tagrot[1],
-                                                                                                     tagrot[2],
-                                                                                                     tag.pose_err))
+        if exit_event.is_set():
+            break
+
+    # print out statistics
+    print("--------------------------------------")
     if len(all_pose_error) > 0:
         print("Pose error (1E8) mean: {0:.3f} and Std dev {1:.3f}".format(numpy.mean(all_pose_error),
                                                                           numpy.std(all_pose_error)))
@@ -109,6 +161,10 @@ def main(args):
         translation_std = numpy.std(posns_array, axis=0)
         print("Tag ID {0} mean: {1} and Std dev {2}".format(tag_id, translation_mean,
                                                             translation_std))
+
+    # close camera
+    for CAMERA in CAMERAS:
+        CAMERA.close()
 
 
 if __name__ == '__main__':

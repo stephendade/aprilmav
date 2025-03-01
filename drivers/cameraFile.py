5b5d34351d6f770e2a5dd9e0341505ea1d12257e
Stephen Dade <stephen_dade@hotmail.com>
2025-03-01 14:03:19 +1100

    Make image enhancement optional

--- drivers/cameraFile.py
+++ drivers/cameraFile.py
@@ -45,7 +45,7 @@ class FileCamera(cameraBase):
         # img = cv2.fastNlMeansDenoising(img,None, 3, 5, 17)
 
         if not get_raw:
-            img = self.doImageEnhancement(img)
+            img = self.maybedoImageEnhancement(img)
             img = self.maybeDoFishEyeConversion(img)
 
         return (img, timestamp)

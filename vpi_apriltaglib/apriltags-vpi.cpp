99943f55af85203d48e796f0bb84c2be609bd26b
Stephen Dade <stephen_dade@hotmail.com>
2025-04-24 22:29:00 +1000

    ApriltagsVPI: Documentation

--- vpi_apriltaglib/apriltags-vpi.cpp
+++ vpi_apriltaglib/apriltags-vpi.cpp
@@ -1,9 +1,22 @@
 /*
-Software library for detecting Apriltags using the NVIDIA VPI (Vision Programming Interface) library.
+ * AprilTag VPI Python Bindings
+ *
+ * Copyright (C) 2025 Stephen Dade <stephen_dade@hotmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-Requires a Jetson with VPI support to run
-
-*/
 #include <iostream>
 
 #include <vpi/OpenCVInterop.hpp>
